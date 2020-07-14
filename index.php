ff6d6dbe5a63b7eee45ad3de81f1ba0cbf1b51f6
Joshua Kenney <joshuagkenney@gmail.com>
2020-07-13 23:23:40 -0400

    tidying up

--- index.php
+++ index.php
@@ -11,11 +11,9 @@ file_put_contents($name, $data);
 <html>
   <head>
     <title>Pessiglione</title>
-    <script src="js/consent.js"></script>
-    <script src="js/functions.js"></script>
-    <script src="jsPsych/jspsych.js"></script>
-    <script src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script>
-    <script src="jsPsych/plugins/jspsych-image-keyboard-response.js"></script>
+    <script type="text/javascript" src="jsPsych/jspsych.js"></script>
+    <script type="text/javascript" src="jsPsych/plugins/jspsych-html-keyboard-response.js"></script>
+    <script type="text/javascript" src="jsPsych/plugins/jspsych-image-keyboard-response.js"></script>
     <link href="jsPsych/css/jspsych.css" rel="stylesheet" type="text/css"></link>
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
@@ -23,8 +21,9 @@ file_put_contents($name, $data);
     <?php include "include/consent.php"?>
   </body>
   <footer>
+    <script src="js/consent.js"></script>
+    <script src="js/functions.js"></script>
     <script type="text/javascript" src="conf.js"></script>
-    <script type="text/javascript" src="timeline.js"></script>
     <script type="text/javascript" src="main.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>
   </footer>
