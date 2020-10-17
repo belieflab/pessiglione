eebfb2683893d4ed887fb9413d47fa9ba93105cd
Joshua Kenney <joshuagkenney@gmail.com>
2020-10-16 21:12:11 -0400

    cursor shows and hides, areYouReady() unloading

--- exp/timeline.js
+++ exp/timeline.js
@@ -250,6 +250,10 @@ let practiceStart = {
       trial_duration: 5000,
       on_finish: function(){
         saveData("pessiglione_" + workerId, jsPsych.data.get().csv());
+        document.getElementById("unload").onbeforeunload='';
+        $(document).ready(function(){
+        $("body").addClass("showCursor"); // returns cursor functionality
+    });
       }
     };
     
