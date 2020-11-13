839940ec7412c404967eeff4f5a2228024200664
Joshua Kenney <joshuagkenney@gmail.com>
2020-11-13 17:04:59 -0500

    italics

--- exp/timeline.js
+++ exp/timeline.js
@@ -301,7 +301,7 @@ let practiceStart = {
       "<p>To leave feedback on this task, please click the following link:</p>"+
       "<p style='color:white;'><a href="+feedbackLink+">Leave Task Feedback!</a></p>"+
       // "<p>Please wait for the experimenter to continue.</p>"+
-      "<p>You may now close the expriment window at anytime.</p>",
+      "<p><i>You may now close the expriment window at anytime.</i></p>",
       choices: jsPsych.NO_KEYS,
       // trial_duration: 60000,
       on_load: function(){
