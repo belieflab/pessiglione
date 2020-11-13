681b508a37598abf5e4839d37cf3f7409847818d
Joshua Kenney <joshuagkenney@gmail.com>
2020-11-12 19:04:57 -0500

    trying to merge

--- db/submit.js
+++ db/submit.js
@@ -1,23 +1,34 @@
 // function to store subject number on submit
 let workerId;
 
-
-// let sexMale;
-// let sexFemale;
-// let age;
-let ageAtAssessment;
-// let age;
-let sexAtBirth;
-// let currentAge;
 let handedness;
-// let antihandedness;
-// let EasyKey_uCase;
-// let HardKey_uCase;
+let antihandedness;
+let EasyKey_uCase;
+let HardKey_uCase;
+
+let ageAtAssessment;
+
+let sexAtBirth;
 let today = new Date();
 let dd = String(today.getDate()).padStart(2, '0');
 let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 let yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
+todayStandard = yyyy + '-' + mm + '-' + dd;
+
+function ageFinder(){
+    if (document.getElementById("dob").value !== '') {
+        let DOB = dob.value;
+        let DOByyyy = DOB.slice(0,4);
+        let DOBmm = DOB.slice(5,7);
+        // let DOBdd = DOB.slice(8,10);
+        let ageInMonths = ((yyyy*12)-(DOByyyy*12)+(mm-DOBmm));
+        ageAtAssessment = parseInt(ageInMonths);
+    } else {
+        alert("Please enter your date of birth.");
+    }
+
+}
 
 function validateIntake() {
     let intake = document.getElementById("intake");
@@ -30,27 +41,42 @@ function validateIntake() {
     }
   }
 
+  function validateSite() {
+    // document.getElementById("sex").value = sex;
+    if (document.getElementById("siteid").value === 'none') {
+        alert("Please select a valid research site.");
+
+    }
+}
+
+function validateBrightness() {
+    // document.getElementById("sex").value = sex;
+    if (document.getElementById("brightness").value == '0') {
+        alert("Please confirm you have turned your screen brightness up to 100%");
+
+    }
+}
+
 function sexFinder() {
     // document.getElementById("sex").value = sex;
     if (document.getElementById("male").checked === true) {
         sexAtBirth = "M";
     } else if (document.getElementById("female").checked === true) {
         sexAtBirth = "F";
+    } 
+}
+
+function validateSex() {
+    if (document.getElementById("male").checked === false && document.getElementById("female").checked === false ) {
+        alert("Please enter the sex you were assigned at birth.");
     }
 }
 
-function ageFinder() {
-    // document.getElementById("sex").value = sex;
-    if (document.getElementById("age").value !== '') {
-        let currentAge = document.getElementById("age").value;
-        ageAtAssessment = parseInt(currentAge);
-    } else {
-        alert("please enter your current age");
-
+function validateHandedness() {
+    if (document.getElementById("rightHanded").checked === false && document.getElementById("leftHanded").checked === false ) {
+        alert("Please select your dominant hand.");
     }
 }
-
-
 
 function submitIntake() {
     let subjectID = document.getElementById("subjectid").value;
@@ -66,34 +92,39 @@ function submitIntake() {
     const zeroPad = (num, places) => String(num).padStart(places, '0');
     switch(siteID.options[siteID.selectedIndex].value){
         case "Maryland":
-            siteNumber = zeroPad(1,2);
+            // siteNumber = zeroPad(1,2);
+            siteNumber = 1;
             break;
         case "Northwestern":
-            siteNumber = zeroPad(2,2);
+            // siteNumber = zeroPad(2,2);
+            siteNumber = 2;
             break;
         case "Temple":
-            siteNumber = zeroPad(3,2);
+            // siteNumber = zeroPad(3,2);
+            siteNumber = 3;
             break;
         case "Georgia":
-            siteNumber = zeroPad(4,2);
+            // siteNumber = zeroPad(4,2);
+            siteNumber = 4;
             break;
         case "Yale":
-            siteNumber = zeroPad(5,2);
+            // siteNumber = zeroPad(5,2);
+            siteNumber = 5;
             break;
         case "Emory":
-            siteNumber = zeroPad(6,2);
+            // siteNumber = zeroPad(6,2);
+            siteNumber = 6;
             break;
         default:
-            siteNumber = zeroPad(0,2);
+            // siteNumber = zeroPad(0,2);
     }
-    // if(siteID.options[siteID.selectedIndex].value == "Yale") {
-    //     siteNumber = "10"
-    // }
+
+  
 
     if(subjectID == "") {
-        alert("Please enter a valid subjectid")
+        alert("Please enter a valid SubjectID.")
     } else {
-        alert("your subjectid is " + siteNumber + subjectID);
+        // alert("your subjectid is " + siteNumber + subjectID);
         workerId = parseInt(siteNumber + subjectID);
         validateIntake();
     }
--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $name = "data/".$post_data['filename'].".csv";
 $data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
+$studyId = $_GET["studyId"];
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +31,8 @@ file_put_contents($name, $data);
     <script type="text/javascript" src="exp/fn.js"></script>
     <script type="text/javascript" src="exp/timeline.js"></script>
     <script type="text/javascript" src="//code.jquery.com/jquery-git.js"></script>
-
+    <script type="text/javascript">
+    let feedbackLink = "https://omnibus.sh/eCRFs/feedback/tasks/pessiglione.php?studyId=<?php echo $studyId?>";
+    </script>
   </footer>
 </html>
